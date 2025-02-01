3a38ab4fcd9f3631e634ec83fcbfb4aec2d07cfb
James Smith <jamesmth@proton.me>
2025-02-01 15:12:42 +0100

    test(tracer): replace ld invocation by gcc

--- crates/nosco-tracer/tests/linux/utils.rs
+++ crates/nosco-tracer/tests/linux/utils.rs
@@ -1,4 +1,4 @@
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::process::Command;
 
 pub fn compile_tracee(
@@ -27,77 +27,32 @@ pub fn compile_tracee(
         .pop()
         .unwrap();
 
-    let mut ld = Command::new("ld");
-    ld.arg("-o").arg(out_file.path());
-
-    if !is_64bit {
-        ld.args(["-m", "elf_i386"]);
-    }
-
-    if is_pie {
-        ld.arg("-pie");
-    }
-
-    ld.arg(o);
-
-    if is_static {
-        ld.arg("-static");
-
-        if is_pie {
-            ld.arg("--no-dynamic-linker")
-                .arg(gcc_path("rcrt1.o", is_64bit));
-        } else {
-            ld.arg(gcc_path("crt1.o", is_64bit));
-        }
-
-        ld.arg(gcc_path("crti.o", is_64bit))
-            .arg("--start-group")
-            .arg(gcc_path("libgcc.a", is_64bit))
-            .arg(gcc_path("libgcc_eh.a", is_64bit))
-            .arg("-lc")
-            .arg("--end-group")
-            .arg(gcc_path("crtn.o", is_64bit));
-    } else {
-        if is_64bit {
-            ld.args(["-dynamic-linker", "/lib64/ld-linux-x86-64.so.2"]);
-        } else {
-            ld.args(["-dynamic-linker", "/lib/ld-linux.so.2"]);
-        }
-
-        ld.arg(gcc_path("Scrt1.o", is_64bit))
-            .arg(gcc_path("crti.o", is_64bit))
-            .arg("-lc")
-            .arg(gcc_path("crtn.o", is_64bit));
-    }
-
-    println!("running: {ld:?}");
-
-    let ld = ld.output().expect("ld");
-
-    if !ld.status.success() {
-        let msg = String::from_utf8_lossy(&ld.stderr);
-        panic!("{msg}");
-    }
-
-    out_file.into_temp_path()
-}
-
-fn gcc_path(filename: &str, is_64bit: bool) -> PathBuf {
     let mut gcc = Command::new("gcc");
+    gcc.arg(o).arg("-o").arg(out_file.path());
 
     if !is_64bit {
         gcc.arg("-m32");
     }
 
-    let output = gcc
-        .arg("--print-file-name")
-        .arg(filename)
-        .output()
-        .expect("gcc");
+    if is_pie && is_static {
+        gcc.arg("-static-pie");
+    } else if is_pie {
+        gcc.arg("-pie");
+    } else {
+        gcc.arg("-no-pie");
+        if is_static {
+            gcc.arg("-static");
+        }
+    }
 
-    std::str::from_utf8(&output.stdout)
-        .expect("utf8")
-        .trim()
-        .to_owned()
-        .into()
+    println!("running: {gcc:?}");
+
+    let gcc = gcc.output().expect("gcc");
+
+    if !gcc.status.success() {
+        let msg = String::from_utf8_lossy(&gcc.stderr);
+        panic!("{msg}");
+    }
+
+    out_file.into_temp_path()
 }
